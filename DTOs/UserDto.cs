cd5713bf2246c79e8925c751621f623b44d272e5
Saumya Reddy <saumyaas2000@yahoo.com>
2025-09-08 07:09:42 +0530

    expense tracker

---
+++ DTOs/UserDto.cs
@@ -0,0 +1,26 @@
+namespace PersonalFinanceTrackerAPI.Domain.DTOs
+{
+    public record UserDto
+    (
+        int Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string FullName,
+    DateTime CreatedAt
+
+    );
+
+    public record CreateUserDto(
+        string FirstName,
+        string LastName,
+        string Email,
+        string PassworHash
+    );
+
+    public record UpdateUserDto(
+        string FirstName,
+        string LastName,
+        string Email
+    );
+}
---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace PersonalFinanceTrackerAPI.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string LasName { get; set; } = string.Empty;
+
+        [Required, EmailAddress, MaxLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
